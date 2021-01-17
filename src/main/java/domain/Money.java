0bc67c7633585d28b217e75747f49fc963e8bd20
nnao45 <n4sekai5y@gmail.com>
2021-01-17 09:07:39 +0900

    Money addition

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -1,6 +1,6 @@
 package domain;
 
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
     Money(int amount, String currency){
@@ -25,5 +25,8 @@ class Money {
     }
     static Money franc(int amount) {
         return new Money(amount, "CHF");
+    }
+    Expression plus(Money append) {
+        return new Money(amount + append.amount, currency);
     }
 }
